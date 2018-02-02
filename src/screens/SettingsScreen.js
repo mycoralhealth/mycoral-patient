946adcc836791f7960a0875937276f35de83abca
Nikola Grcevski <ngrcevski@venasolutions.com>
2018-02-02 14:09:46 -0500

    Add some basic demo settings screen.

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import { View } from 'react-native';
+import { List, ListItem } from 'react-native-elements'
+
 
 import { CoralHeader, colors } from '../ui.js';
+
+doNothing = (menuItem) => {
+  alert(`${menuItem.title} activated. This is a demo.`);
+}
 
 export const SettingsScreen = () => (
   <View style={{ flex: 1, backgroundColor: colors.bg  }}>
     <CoralHeader title='Account Settings' subtitle='Configure blockchain account settings.'/>
 
-
+    <List containerStyle={{marginTop: 0, marginBottom: 20, borderTopWidth: 0, borderBottomWidth: 0}}>
+      {
+        [
+          {"icon":"user", "title":"Profile"},
+          {"icon":"paint-brush", "title":"Appearance"},
+          {"icon":"bell", "title":"Notifications"},
+          {"icon":"circle", "title":"Account"}
+        ].map((item, i) => (
+          <ListItem
+            key={i}
+            title={item.title}
+            chevronColor={colors.red}
+            leftIcon={{name:item.icon, type:'font-awesome', color: '#ddd'}}
+            onPress={() => doNothing(item)}
+          />
+        ))
+      }
+    </List>
   </View>
 );
