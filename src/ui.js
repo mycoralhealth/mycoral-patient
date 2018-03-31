87f497173fb0ecf5918a73c9b3305d165114f104
kriscosta <kristoncosta@gmail.com>
2018-03-31 16:51:59 -0400

    Fixing spacing for iphone x notch

--- src/ui.js
+++ src/ui.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { View, Image, Platform, Modal, TouchableOpacity, ActivityIndicator } from 'react-native';
 import { Text, Button, Icon, List, ListItem } from 'react-native-elements';
 import { PHOTO_RECORD_TEST } from './utilities/recordTypes';
-import { NavigationActions } from 'react-navigation';
+import { NavigationActions, SafeAreaView } from 'react-navigation';
+
 
 import store from './utilities/store';
 import { cleanUpRecordsCache } from './screens/MyRecordsScreen';
@@ -36,15 +37,17 @@ export const logoutAction = async (navigation) => {
 export class CoralHeader extends React.Component {
   render() {
     return (
-      <View style={{ flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: colors.green, borderTopWidth: 1, borderTopColor: colors.green, marginTop: (Platform.OS === 'ios') ? 20 : 0}}>
-        <View style={{ backgroundColor: 'white', width: 80, height: 80, justifyContent: 'center'}}>
-          <Image style={{ width: 80, height: 80 }} source={require('../assets/corner-logo.png')} />
+      <SafeAreaView forceInset={{top:'always'}}>
+        <View style={{ flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: colors.green, borderTopWidth: 1, borderTopColor: colors.green, marginTop: (Platform.OS === 'ios') ? 20 : 0}}>
+          <View style={{ backgroundColor: 'white', width: 80, height: 80, justifyContent: 'center'}}>
+            <Image style={{ width: 80, height: 80 }} source={require('../assets/corner-logo.png')} />
+          </View>
+          <View style={{ backgroundColor: colors.green, height: 80, padding: 20, flex: 1, justifyContent: 'center'}}>
+            <Text style={{ fontSize: 20, color: "white", textAlign: "center" }}>{this.props.title}</Text>
+            <Text style={{ fontSize: 12, color: 'rgba(255, 255, 255, 0.6)', textAlign: "center" }}>{this.props.subtitle}</Text>
+          </View>
         </View>
-        <View style={{ backgroundColor: colors.green, height: 80, padding: 20, flex: 1, justifyContent: 'center'}}>
-          <Text style={{ fontSize: 20, color: "white", textAlign: "center" }}>{this.props.title}</Text>
-          <Text style={{ fontSize: 12, color: 'rgba(255, 255, 255, 0.6)', textAlign: "center" }}>{this.props.subtitle}</Text>
-        </View>
-      </View>
+      </SafeAreaView>
     );
   }
 }
