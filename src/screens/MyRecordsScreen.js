3f00e4c7aec294477de219e4685ea69b50aa6cfa
kriscosta <kristoncosta@gmail.com>
2018-04-22 13:24:19 -0400

    Whoops, added a bracket.

--- src/screens/MyRecordsScreen.js
+++ src/screens/MyRecordsScreen.js
@@ -137,7 +137,7 @@ class MyRecordsScreenUnwrapped extends AsyncRenderComponent {
       cachedRecords = await store.records();
     }
     this.setState({recordsList: cachedRecords, loading: false});
-    this.decryptRecords(cachedRecords);)
+    this.decryptRecords(cachedRecords);
   }
 
   async decryptRecords(recordsList) {
