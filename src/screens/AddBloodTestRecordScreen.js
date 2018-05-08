3185a180b4dd61ffb0b420f2c6529910779ff226
jadengis <jadengis@gmail.com>
2018-05-07 23:27:33 -0400

    refactor blood test screen

--- src/screens/AddBloodTestRecordScreen.js
+++ src/screens/AddBloodTestRecordScreen.js
@@ -1,15 +1,16 @@
-import moment from 'moment'
-import React from 'react';
-import { View } from 'react-native';
-import { Button, List, ListItem, Text } from 'react-native-elements'
-import { NavigationActions } from 'react-navigation';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import moment from "moment";
+import React from "react";
+import { View } from "react-native";
+import { Button, List, ListItem, Text } from "react-native-elements";
+import { NavigationActions } from "react-navigation";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
-import { colors } from '../ui';
-import { TestRecordScreen } from './TestRecordScreen';
+import { colors } from "../ui";
+import { TestRecordScreen } from "./TestRecordScreen";
 
-import { BLOOD_TEST, recordTypes } from '../utilities/recordTypes';
-import {MyRecordsScaffold} from "./MyRecordsScaffold";
+import { BLOOD_TEST, recordTypes } from "../utilities/recordTypes";
+import { MyRecordsScaffold } from "./MyRecordsScaffold";
+import { AddRecordView } from "./AddRecordView";
 
 const backAction = NavigationActions.back();
 
@@ -18,10 +19,10 @@ export class AddBloodTestRecordScreen extends TestRecordScreen {
     super(props);
 
     this.state = {
-      "Cholesterol" : "",
-      "HbA1c" : "",
-      "hsCRP" : "",
-      "opInProgress": false
+      Cholesterol: "",
+      HbA1c: "",
+      hsCRP: "",
+      opInProgress: false
     };
   }
 
@@ -32,18 +33,33 @@ export class AddBloodTestRecordScreen extends TestRecordScreen {
   }
 
   async addRecord() {
-    this.setState({opInProgress: true});
-    
+    this.setState({ opInProgress: true });
+
     let results = [
-      {"key": "Cholesterol", "value": this.state['Cholesterol'], "type":"marker", "valueType":"magnitude"},
-      {"key": "HbA1c", "value": this.state['HbA1c'], "type":"marker", "valueType":"magnitude"},
-      {"key": "hsCRP", "value": this.state['hsCRP'], "type":"marker", "valueType":"magnitude"}
+      {
+        key: "Cholesterol",
+        value: this.state["Cholesterol"],
+        type: "marker",
+        valueType: "magnitude"
+      },
+      {
+        key: "HbA1c",
+        value: this.state["HbA1c"],
+        type: "marker",
+        valueType: "magnitude"
+      },
+      {
+        key: "hsCRP",
+        value: this.state["hsCRP"],
+        type: "marker",
+        valueType: "magnitude"
+      }
     ];
 
     try {
       let record = await this.createRecord(JSON.stringify(results), BLOOD_TEST);
       this.props.navigation.state.params.onRecordAdded(record);
-    } catch(e) {
+    } catch (e) {
       this.props.navigation.state.params.onRecordAddFailed();
     } finally {
       this.props.navigation.dispatch(backAction);
@@ -52,57 +68,34 @@ export class AddBloodTestRecordScreen extends TestRecordScreen {
 
   render() {
     return (
-      <MyRecordsScaffold
-        title={`Add ${recordTypes[BLOOD_TEST]}`}
-        subtitle='Enter your results below.'
-        backAction={() => this.props.navigation.dispatch(backAction)}>
-        <KeyboardAwareScrollView style={{ flex: 1 }}>
-          <Text h3 style={{textAlign: 'center', marginTop: 20}}>
-            {recordTypes[BLOOD_TEST]}
-          </Text>
-          <Text style={{textAlign: 'center'}}>
-            Date: {moment().format('MMMM Do, YYYY')}
-          </Text>
-
-          <List containerStyle={{marginBottom: 20}}>
-            {
-              [
-                {"key":"Cholesterol","placeholder":"180"},
-                {"key":"HbA1c","placeholder":"6.0"},
-                {"key":"hsCRP","placeholder":"2.5"}
-              ].map((item, index) => (
-
-                <ListItem
-                  key={item.key}
-                  title={item.key}
-                  hideChevron={true}
-                  textInput={true}
-                  textInputPlaceholder={item.placeholder}
-                  textInputValue={this.state[item.key]}
-                  textInputOnChangeText={(title) => this.onChangeValue(item.key, title)}
-                  textInputKeyboardType='numeric'
-                  textInputReturnKeyType={'done'}
-                />
-              ))
-            }
-          </List>
-
-          <View style={{ flex: 1}}>
-            <View style={{ flex: 1, marginBottom: 10}}>
-              <Button
-                disabled={this.state.opInProgress}
-                backgroundColor={colors.green}
-                icon={{name: 'ios-add-circle', type: 'ionicon'}}
-                title='Save'
-                onPress={async () => this.addRecord()}
-              />
-            </View>
-          </View>
-
-          <View style={{ height: 200 }}/>
-          
-        </KeyboardAwareScrollView>
-      </MyRecordsScaffold>
+      <AddRecordView
+        recordType={recordTypes[BLOOD_TEST]}
+        navigation={this.props.navigation}
+        opInProgress={this.state.opInProgress}
+        saveAction={async () => this.addRecord()}
+      >
+        <List containerStyle={{ marginBottom: 20 }}>
+          {[
+            { key: "Cholesterol", placeholder: "180" },
+            { key: "HbA1c", placeholder: "6.0" },
+            { key: "hsCRP", placeholder: "2.5" }
+          ].map((item, index) => (
+            <ListItem
+              key={item.key}
+              title={item.key}
+              hideChevron={true}
+              textInput={true}
+              textInputPlaceholder={item.placeholder}
+              textInputValue={this.state[item.key]}
+              textInputOnChangeText={title =>
+                this.onChangeValue(item.key, title)
+              }
+              textInputKeyboardType="numeric"
+              textInputReturnKeyType={"done"}
+            />
+          ))}
+        </List>
+      </AddRecordView>
     );
   }
 }
